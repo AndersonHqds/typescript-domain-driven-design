db4c0a6632ee82923a8a131a604da2fbce61f717
Anderson Silva <contatoahenrique@gmail.com>
2023-12-12 08:42:25 -0300

    fix: Fix unit tests

--- src/infrastructure/repository/customer.repository.spec.ts
+++ src/infrastructure/repository/customer.repository.spec.ts
@@ -71,7 +71,7 @@ describe("Customer repository test", () => {
     const customerRepository = new CustomerRepository();
     const customer = new Customer("123", "Customer 1");
     const address = new Address("Street 1", 1, "Zipcode 1", "City 1");
-    customer.address = address;
+    customer.changeAddress(address);
     await customerRepository.create(customer);
 
     const customerResult = await customerRepository.find(customer.id);
@@ -91,13 +91,13 @@ describe("Customer repository test", () => {
     const customerRepository = new CustomerRepository();
     const customer1 = new Customer("123", "Customer 1");
     const address1 = new Address("Street 1", 1, "Zipcode 1", "City 1");
-    customer1.address = address1;
+    customer1.changeAddress(address1);
     customer1.addRewardPoints(10);
     customer1.activate();
 
     const customer2 = new Customer("456", "Customer 2");
     const address2 = new Address("Street 2", 2, "Zipcode 2", "City 2");
-    customer2.address = address2;
+    customer2.changeAddress(address2);
     customer2.addRewardPoints(20);
 
     await customerRepository.create(customer1);
@@ -106,7 +106,6 @@ describe("Customer repository test", () => {
     const customers = await customerRepository.findAll();
 
     expect(customers).toHaveLength(2);
-    expect(customers).toContainEqual(customer1);
-    expect(customers).toContainEqual(customer2);
+    expect(customers).toStrictEqual([customer1, customer2]);
   });
 });
